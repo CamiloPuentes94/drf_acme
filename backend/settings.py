37bf6596910589d8e88bfb3ba8a2f463a55f42ce
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2023-01-27 22:57:09 -0500

    Se realiza la activacion de los cors para permitir la comunicacion entre fronted y la API

--- backend/settings.py
+++ backend/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    
+    "corsheaders",
 ]
 
 REST_FRAMEWORK = {
@@ -86,6 +88,9 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    "corsheaders.middleware.CorsMiddleware",
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -166,4 +171,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
